acb0636a4375f95d31979eecf17b1ca09652f465
Ralf Schoch-Funkenhaus <ralf.schoch-funkenhaus@telekom.de>
2020-05-29 14:35:50 +0200

    bis Hint 22 MainActivity Läuft :-)

--- app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
+++ app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
@@ -1,14 +1,56 @@
 package de.telekom.sea.mystuff.frontend.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import de.telekom.sea.mystuff.frontend.android.databinding.MyStuffItemBinding;
+import de.telekom.sea.mystuff.frontend.android.ui.ItemListRecyclerViewAdapter;
+import de.telekom.sea.mystuff.frontend.android.util.ItemListViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout refreshLayout;
+    private ItemListViewModel viewModel;
+    private ItemListRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MyStuffItemBinding myStuffItemBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(ItemListViewModel.class);
+        viewModel.initWithApplication(getApplication());
+
+        // bind RecyclerView
+        recyclerView = findViewById(R.id.rv_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ItemListRecyclerViewAdapter(new ArrayList<>());
+        recyclerView.setAdapter(adapter);
+
+        getAllItems();
     }
+
+    private void getAllItems() {
+        viewModel.getItems().observe(this, apiResponse -> {
+            if (apiResponse.isSuccessful()) {
+                adapter.updateItemList(apiResponse.body);
+            } else {
+                ((MyStuffApplication) getApplication()).getMyStuffContext().sendInfoMessage(apiResponse.errorMessage);
+            }
+        });
+    }
+
+
 }
